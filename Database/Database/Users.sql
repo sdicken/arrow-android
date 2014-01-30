ff4d27a26e422d9d002f7d68a9eff5aa4ed6780d
Thisita <thisita@outlook.com>
2014-01-30 16:40:33 -0500

    Initial commit for database project. Includes Users table

---
+++ Database/Database/Users.sql
@@ -0,0 +1,78 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Username] NTEXT NOT NULL UNIQUE, 
+    [Secret] NTEXT NOT NULL, 
+    [Email] NTEXT NULL, 
+    [Street] NTEXT NULL, 
+    [City] NTEXT NULL DEFAULT 'Louisville', 
+    [State] NTEXT NULL DEFAULT 'KY', 
+    [Zip] NUMERIC(5) NULL DEFAULT 40208, 
+    [TotalOrders] INT NOT NULL DEFAULT 0, 
+    [TotalPaid] MONEY NOT NULL DEFAULT 0.00, 
+    [LastOrder] TIMESTAMP NULL
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Username for the user that will be used to log into the system.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Users',
+    @level2type = N'COLUMN',
+    @level2name = N'Username'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Base64 encoded salt and hash of the authentication key for the user.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Users',
+    @level2type = N'COLUMN',
+    @level2name = N'Secret'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Primary email address of the user.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Users',
+    @level2type = N'COLUMN',
+    @level2name = N'Email'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The street address (ie: 123 Sherman Way Apt #345b) the user is to be delivered at.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Users',
+    @level2type = N'COLUMN',
+    @level2name = N'Street'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The city the user is located for delivery to.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Users',
+    @level2type = N'COLUMN',
+    @level2name = N'City'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The state of the address of the user to be delivered to.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Users',
+    @level2type = N'COLUMN',
+    @level2name = N'State'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The ZIP code of the address the user is to be delivered at.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Users',
+    @level2type = N'COLUMN',
+    @level2name = N'Zip'
