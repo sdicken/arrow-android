01914d795a9bc8be82194c0ab676481d21fe6321
Thisita <thisita@outlook.com>
2014-01-30 20:10:19 -0500

    Added GPSUpdates table to store driver GPS updates. Why driver only? You try storing fine location data for your users... let's see what kind of jail you sit in afterwards.
    Related Work Items: #19

---
+++ Database/Database/GPSUpdates.sql
@@ -0,0 +1,54 @@
+﻿CREATE TABLE [dbo].[GPSUpdates]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [DriverId] INT NOT NULL, 
+    [Latitude] FLOAT NOT NULL, 
+    [Longitude] FLOAT NOT NULL, 
+    [Timestamp] TIMESTAMP NOT NULL
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The unique id of the GPS update',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'GPSUpdates',
+    @level2type = N'COLUMN',
+    @level2name = N'Id'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The id of the driver that this location belongs to',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'GPSUpdates',
+    @level2type = N'COLUMN',
+    @level2name = N'DriverId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The latitudnal value of the location of the driver',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'GPSUpdates',
+    @level2type = N'COLUMN',
+    @level2name = N'Latitude'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The longitudnal value of the location of the driver',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'GPSUpdates',
+    @level2type = N'COLUMN',
+    @level2name = N'Longitude'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The time at which this data was recorded',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'GPSUpdates',
+    @level2type = N'COLUMN',
+    @level2name = N'Timestamp'
