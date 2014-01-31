62cae8b69eb887dcb005d8dc7ac9c76520efff9b
Thisita <thisita@outlook.com>
2014-01-30 19:37:07 -0500

    Added restrauntid and items total to the orders table. Added column comments.
    Related Work Items: #19

--- Database/Database/Orders.sql
+++ Database/Database/Orders.sql
@@ -6,5 +6,80 @@
     [DeliveryTime] TIMESTAMP NULL, 
     [DriverId] INT NOT NULL, 
     [ClientId] INT NOT NULL, 
-    [OrderTotal] MONEY NOT NULL
+    [OrderTotal] MONEY NOT NULL, 
+    [ItemsTotal] INT NOT NULL, 
+    [RestrauntId] INT NOT NULL
 )
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The time that the order was submitted.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Orders',
+    @level2type = N'COLUMN',
+    @level2name = N'SubmitTime'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The time the driver picked up the order from the restraunt.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Orders',
+    @level2type = N'COLUMN',
+    @level2name = N'PickUpTime'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The time the driver delivered the order to the customer.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Orders',
+    @level2type = N'COLUMN',
+    @level2name = N'DeliveryTime'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The User Id of the driver.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Orders',
+    @level2type = N'COLUMN',
+    @level2name = N'DriverId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The User Id of the client.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Orders',
+    @level2type = N'COLUMN',
+    @level2name = N'ClientId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The total value of the order',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Orders',
+    @level2type = N'COLUMN',
+    @level2name = N'OrderTotal'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The total number of items in the order.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Orders',
+    @level2type = N'COLUMN',
+    @level2name = N'ItemsTotal'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The restraunt that the order items  are from',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Orders',
+    @level2type = N'COLUMN',
+    @level2name = N'RestrauntId'
