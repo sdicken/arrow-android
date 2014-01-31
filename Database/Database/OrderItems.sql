7676fddc53a6645f2e948cb062257e92f2ef9123
Thisita <thisita@outlook.com>
2014-01-30 19:46:35 -0500

    Added orderitems table for tracking the items on an order.
    Related Work Items: #19

---
+++ Database/Database/OrderItems.sql
@@ -0,0 +1,64 @@
+﻿CREATE TABLE [dbo].[OrderItems]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [OrderId] INT NOT NULL, 
+    [ItemId] INT NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    [TotalPrice] MONEY NOT NULL, 
+    [Comments] NTEXT NULL
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The unique id for the order item',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'OrderItems',
+    @level2type = N'COLUMN',
+    @level2name = N'Id'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The Id of the order that the item(s) are on.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'OrderItems',
+    @level2type = N'COLUMN',
+    @level2name = N'OrderId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The Id of the item being ordered.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'OrderItems',
+    @level2type = N'COLUMN',
+    @level2name = N'ItemId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The number of Items ordered.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'OrderItems',
+    @level2type = N'COLUMN',
+    @level2name = N'Quantity'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The total price of items ordered',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'OrderItems',
+    @level2type = N'COLUMN',
+    @level2name = N'TotalPrice'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Any comments relating to this item(s). This can be information such as what toppings should go on a sandwhich. The Client User should be warned that they will be responsible for possible increases in the price of their order due to the input given here.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'OrderItems',
+    @level2type = N'COLUMN',
+    @level2name = N'Comments'
