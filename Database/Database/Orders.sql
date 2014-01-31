5c462c2af7b973092215cf908500f17ababd1c86
Thisita <thisita@outlook.com>
2014-01-30 19:32:19 -0500

    Added Orders Table

---
+++ Database/Database/Orders.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [SubmitTime] TIMESTAMP NOT NULL, 
+    [PickUpTime] TIMESTAMP NULL, 
+    [DeliveryTime] TIMESTAMP NULL, 
+    [DriverId] INT NOT NULL, 
+    [ClientId] INT NOT NULL, 
+    [OrderTotal] MONEY NOT NULL
+)
