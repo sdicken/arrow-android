349629da5a93e3ca71c5197c745e831dd1f66124
Thisita <thisita@outlook.com>
2014-02-18 18:46:38 -0500

    Parsing of account/edit page to get user information. yay hacks.
    Some stubs, but most things are actually done.

--- Android/ArrowFood/Arrow Food/src/main/java/com/arrowfoodcouriers/arrowfood/OpenCart/OpenCartSession.java
+++ Android/ArrowFood/Arrow Food/src/main/java/com/arrowfoodcouriers/arrowfood/OpenCart/OpenCartSession.java
@@ -10,6 +10,7 @@ public class OpenCartSession {
     public final String Server = "http://192.168.1.185/";
     public final String LoginRoute = "index.php?route=account/login";
     public final String RegisterRoute = "index.php?route=account/register";
+    public final String EditAccountRoute = "index.php?route=account/edit";
     public final String LogoutRoute = "index.php?route=account/logout"; // TODO: Check me!
     public final String OrderRoute = "index.php?route=order/checkout"; // TODO: Check me!
 
@@ -17,10 +18,42 @@ public class OpenCartSession {
     private String _email;
     private Boolean _authenticated;
 
-    private String DoRequest(URL url, String body) throws IOException, ExecutionException, InterruptedException {
+    private String _firstName;
+    private String _lastName;
+    private String _telephone;
+
+    private String DoPOST(URL url, String body) throws IOException, ExecutionException, InterruptedException {
         POSTCall request = new POSTCall();
         request.execute(url, body, _cookieManager);
         return request.get();
+    }
+
+    private String DoGET(URL url) throws IOException, ExecutionException, InterruptedException {
+        GETCall request = new GETCall();
+        request.execute(url, _cookieManager);
+        return request.get();
+    }
+
+    private final Integer FirstNameLine = 156;
+    private final Integer LastNameLine = 161;
+    private final Integer TelephoneLine = 171;
+    private final Integer FormControlEndCharOffset = 4;
+
+    private void ParseEditHTML(String html) {
+        String firstName, lastName, phone;
+        String[] lines = html.split("\n");
+
+        firstName = lines[FirstNameLine];
+        lastName = lines[LastNameLine];
+        phone = lines[TelephoneLine];
+
+        firstName = firstName.substring(firstName.indexOf("value=\""), firstName.length() - FormControlEndCharOffset);
+        lastName = lastName.substring(lastName.indexOf("value=\""), lastName.length() - FormControlEndCharOffset);
+        phone = phone.substring(phone.indexOf("value=\""), phone.length() - FormControlEndCharOffset);
+
+        _firstName = firstName;
+        _lastName = lastName;
+        _telephone = phone;
     }
 
     public OpenCartSession() {
@@ -30,6 +63,18 @@ public class OpenCartSession {
 
     public String GetEmail() {
         return _email;
+    }
+
+    public String GetFirstName() {
+        return _firstName;
+    }
+
+    public String GetLastName() {
+        return _lastName;
+    }
+
+    public String GetTelephone() {
+        return _telephone;
     }
 
     public Boolean IsAuthenticated() {
@@ -44,7 +89,7 @@ public class OpenCartSession {
             json += "\", \"password\": \"";
             json += password;
             json += "\"}";
-            DoRequest(url, json);
+            DoPOST(url, json);
             _email = email;
             _authenticated = true;
             Log.d("Login", "Logged in");
@@ -62,7 +107,7 @@ public class OpenCartSession {
         }
         try {
             URL url = new URL(Server + RegisterRoute);
-            DoRequest(url, registration.GetJson());
+            DoPOST(url, registration.GetJson());
             _email = registration.Email;
             _authenticated = true;
             return true;
@@ -76,11 +121,11 @@ public class OpenCartSession {
 
         try {
             URL url = new URL(Server + LogoutRoute);
-            DoRequest(url, "");
+            DoPOST(url, "");
             _email = null;
             _authenticated = false;
         } catch (Exception ex) {
-            Log.e("Logout", "Caught exception!");
+            Log.e("Logout()", "Caught exception!");
         }
     }
 
@@ -90,10 +135,22 @@ public class OpenCartSession {
         }
         try {
             URL url = new URL(Server + OrderRoute);
-            DoRequest(url, order.GetJson());
+            DoPOST(url, order.GetJson());
             return true;
         } catch (Exception ex) {
             return false;
+        }
+    }
+
+    public void LoadUserData() {
+        if (!_authenticated) return;
+
+        try {
+            URL url = new URL(Server + EditAccountRoute);
+            String response = DoGET(url);
+            ParseEditHTML(response);
+        } catch (Exception ex) {
+            Log.e("LoadUserData()", "Caught exception!");
         }
     }
 }
