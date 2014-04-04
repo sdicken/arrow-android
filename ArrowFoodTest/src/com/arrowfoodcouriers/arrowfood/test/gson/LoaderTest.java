03edc26d37a9b1d5c31db6b02611f9a77a096622
Sam Dicken <sjdick04@louisville.edu>
2014-04-03 23:49:13 -0400

    Add test case for UserAccountLoader that was written inside MainActivity class.

---
+++ ArrowFoodTest/src/com/arrowfoodcouriers/arrowfood/test/gson/LoaderTest.java
@@ -0,0 +1,31 @@
+package com.arrowfoodcouriers.arrowfood.test.gson;
+
+import java.util.Date;
+
+import org.hamcrest.core.IsEqual;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import com.arrowfoodcouriers.arrowfood.Models.User;
+import com.arrowfoodcouriers.arrowfood.gson.UserAccountLoader;
+
+@RunWith(RobolectricTestRunner.class)
+public class LoaderTest 
+{
+	@Test
+	public void testLoadedDataMatchesSavedData()
+	{
+		UserAccountLoader loader = new UserAccountLoader(Robolectric.getShadowApplication().getApplicationContext());
+        User testUser = new User("test", "customer", "test@test.test", "Tester Test", "123 Fake Address", "", "Louisville", "KY", "40208", new Date().getTime());
+        loader.saveData(testUser);
+        User savedUser = loader.loadData();
+        assertTrue(testUser.equals(savedUser));
+	}
+}
