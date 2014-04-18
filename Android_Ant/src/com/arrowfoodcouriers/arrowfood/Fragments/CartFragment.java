dbbb814c0c0e0cbaf52ec08a53a4fd3bb7c3557a
Sam Dicken <sjdick04@louisville.edu>
2014-04-18 16:18:54 -0400

    Add click handler for button to load CheckoutFragment

--- Android_Ant/src/com/arrowfoodcouriers/arrowfood/Fragments/CartFragment.java
+++ Android_Ant/src/com/arrowfoodcouriers/arrowfood/Fragments/CartFragment.java
@@ -4,11 +4,14 @@ import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.arrowfoodcouriers.arrowfood.R;
+import com.arrowfoodcouriers.arrowfood.Utils;
 import com.arrowfoodcouriers.arrowfood.Adapter.CartAdapter;
 
 public class CartFragment extends ListFragment 
@@ -24,9 +27,21 @@ public class CartFragment extends ListFragment
 		TextView subtotalText = (TextView) view.findViewById(R.id.cart_subtotal_value);
 //		subtotalText.setText("$" + String.valueOf(Utils.getCart().getTotal()));
 		
+		Button checkoutButton = (Button) view.findViewById(R.id.cart_checkout_button);
+		checkoutButton.setOnClickListener(new CartCheckoutButtonListener());
+		
 		mAdapter = new CartAdapter();
 		setListAdapter(mAdapter);
 		
 		return view;
+	}
+	
+	private class CartCheckoutButtonListener implements OnClickListener
+	{
+		@Override
+		public void onClick(View v) 
+		{
+			Utils.loadFragment(getFragmentManager(), new CheckoutFragment());			
+		}
 	}
 }
