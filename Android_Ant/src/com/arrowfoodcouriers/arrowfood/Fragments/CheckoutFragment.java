c50ffa52792472e982103239fc3389aea35baed9
Sam Dicken <sjdick04@louisville.edu>
2014-04-18 23:51:59 -0400

    Add PayPal fake payment made in sandbox mode.

--- Android_Ant/src/com/arrowfoodcouriers/arrowfood/Fragments/CheckoutFragment.java
+++ Android_Ant/src/com/arrowfoodcouriers/arrowfood/Fragments/CheckoutFragment.java
@@ -2,9 +2,13 @@ package com.arrowfoodcouriers.arrowfood.Fragments;
 
 import java.math.BigDecimal;
 
+import org.json.JSONException;
+
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,9 +18,12 @@ import android.widget.Button;
 import com.arrowfoodcouriers.arrowfood.R;
 import com.paypal.android.sdk.payments.PayPalPayment;
 import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 
 public class CheckoutFragment extends Fragment
 {
+	private static final String PAYPAL = "paypal";
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) 
@@ -35,10 +42,32 @@ public class CheckoutFragment extends Fragment
 		public void onClick(View v) 
 		{
 			Double price = Double.valueOf(7.35);
-			PayPalPayment payment = new PayPalPayment(new BigDecimal(price), "US", "Arrow Food Couriers", PayPalPayment.PAYMENT_INTENT_SALE);
+			PayPalPayment payment = new PayPalPayment(new BigDecimal(price), "USD", "Arrow Food Couriers", PayPalPayment.PAYMENT_INTENT_SALE);
 			Intent intent = new Intent(getActivity(), PaymentActivity.class);
 			intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
 			startActivityForResult(intent, 0);
 		}
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) 
+	{
+		if(resultCode == Activity.RESULT_OK)
+    	{
+    		PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+            if (confirm != null) 
+            {
+                try {
+                    Log.i(PAYPAL, confirm.toJSONObject().toString(4));
+
+                    // TODO: send 'confirm' to your server for verification.
+                    // see https://developer.paypal.com/webapps/developer/docs/integration/mobile/verify-mobile-payment/
+                    // for more details.
+
+                } catch (JSONException e) {
+                    Log.e(PAYPAL, "an extremely unlikely failure occurred: ", e);
+                }
+            }
+    	}
 	}
 }
